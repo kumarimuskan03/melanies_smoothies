f2087e3ac095fb28b147a0860f46580a8b696e42
Muskan Kumari <95340861+kumarimuskan03@users.noreply.github.com>
2024-05-09 13:03:41 +0530

    Update streamlit_app.py
    change cnx bcz of module error

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.write(
     
 )
 
-cnx=st.connection("snowflake")
+cnx = st.snowflake()
 session =cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
