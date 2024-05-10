fa0ebe9c95f2c6a26b814aeabb4ae0bafd006c4f
Muskan Kumari <95340861+kumarimuskan03@users.noreply.github.com>
2024-05-10 15:28:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,10 @@
+  # Import python packages
 import streamlit as st
-import requests
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-
-
-
  
 # Write directly to the app
-st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
+st.title(":cup_w    ith_straw: Customize your smoothie :cup_with_straw:")
 st.write(
     """Choose the fruits you want from your custom Smoothie!
     """)
@@ -16,14 +14,9 @@ st.write("The name on your Smoothie will be:", name_on_order)
  
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-#convert snowflake datafram into pandas dataframe
-pd_df=my_dataframe.to_pandas()
-# st.dataframe(pd_df)
-# st.stop()
-
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+ 
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients:'
     ,my_dataframe
@@ -37,11 +30,6 @@ if ingredients_list:
  
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+search_on)
-        fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
  
     #st.write(ingredients_string)  
  
@@ -56,4 +44,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '  +name_on_order+ '!', icon="✅")
-
